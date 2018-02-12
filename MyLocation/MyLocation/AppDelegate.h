5a859d6e52c6a66f5a84b93a5788774aeb221019
周信明 <mingxz100@163.com>
2018-02-12 18:20:04 +0800

    Add files via upload

---
+++ MyLocation/MyLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLocation
+//
+//  Created by Zhouxm on 2018/2/11.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLocation/MyLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLocation
+//
+//  Created by Zhouxm on 2018/2/11.
+//  Copyright © 2018年 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
